b3085e1c52e8ded487d96fe5b189047e9cd57d76
matthewsullivan1 <143414874+matthewsullivan1@users.noreply.github.com>
2023-10-13 11:50:10 -0500

    Update sales.c

--- sales.c
+++ sales.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
 char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
 void monthlySales(float sales[]){
-    printf("Monthly sales report for 2022:\nMonth\tSales\n");
+    printf("Monthly sales report for 2022:\nMonth\t\tSales\n");
 
     for(int i = 0; i<12; i++){
-        printf("%s \t %f \n", months[i], sales[i]);
+        printf("%s \t $%.2f \n", months[i], sales[i]);
     }
+}
+int compareSales(const void *a, const void *b){
+    return (*(float *)b - *(float *)a);
 }
 
 void salesSummary(float sales[]){
-    int min_month;
-    int max_month;
+    int min_month = 0;
+    int max_month = 0;
     
     float min = sales[0];
     float max = sales[0];
     float avg = 0;
-
+    
+    printf("\nSales Summary:\n\n");
     for (int i = 0; i < 12; i++) {     
         if(sales[i] < min){
             min = sales[i];
             min_month = i;
-            printf("%f < %f\n", sales[i], min);
         }    
         if(sales[i] > max){  
             max = sales[i];
@@ -29,19 +34,48 @@ void salesSummary(float sales[]){
         }
         avg += sales[i];    
     }
-
-
-    
-    printf("Minimun sales:\t$%f (%s)\n", min, months[min_month]);
-    printf("Maximun sales:\t$%f (%s)\n", max, months[max_month]);
-    printf("Average sales:\t$%f\n", avg/12);
-
+    printf("Minimun sales:\t$%.2f (%s)\n", min, months[min_month]);
+    printf("Maximun sales:\t$%.2f (%s)\n", max, months[max_month]);
+    printf("Average sales:\t$%.2f\n\n", avg/12);
 }
+void sixMonthReport(float sales[]){
+    printf("\nSix-Month Moving Average Report:\n\n");
+    for (int i = 0; i < 7; i++){
+        float avg = 0;
 
+        for (int j = 0; j < 6; j++){
+            avg += sales[j+i];            
+        }
+        printf("%s-%s\t$%.2f\n", months[0+i],months[5+i], avg/6);
+    }
+}
+void highestToLowest(float sales[]){
+    char months2[12][15] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    char temp[15];
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12 - i; j++){
+            if (sales[j] < sales[j+1]){    
+                float tmp = sales[j];
+                sales[j] = sales[j+1];
+                sales[j+1] = tmp;
+
+  
+                strcpy(temp, months2[j]);
+                strcpy(months2[j], months2[j+1]);
+                strcpy(months2[j+1], temp);
+            }
+        }
+    }
+
+    printf("\nSales Report (Highest to Lowest):\n\nMonth\t\tSales\n");
+    for(int i = 0; i < 12; i++){
+        printf("%s\t$%.2f\n", months2[i],sales[i]);
+    }
+}
 
 int main(){
 
-    
     FILE* file = fopen ("data.txt", "r");
     float sales[100]; 
 
@@ -56,10 +90,8 @@ int main(){
 
     monthlySales(sales);
     salesSummary(sales);
-
-    
-
-
+    sixMonthReport(sales);
+    highestToLowest(sales);
 
     return 0;
 }
